3216707df647d8b3697dcb50b2dc25090ee4cef0
Reyyan Kucukkara <reyyankucukkara42@gmail.com>
2026-02-01 16:27:53 +0300

    Son işlemler tamamlandı

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,15 @@
+//routes/auth.js
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-// TEST: GET http://localhost:3000/api/auth/test
 router.get("/test", (req, res) => {
     res.json({ success: true, message: "Auth Route Calisiyor" });
 });
 
-// LOGIN: POST http://localhost:3000/api/auth/login
-// NOT: Buradaki virgüllere ve fonksiyon isimlerine dikkat edin
 router.post("/login", authController.login);
 
-// REGISTER: POST http://localhost:3000/api/auth/register
+
 router.post("/register", authController.register);
 
 module.exports = router;
